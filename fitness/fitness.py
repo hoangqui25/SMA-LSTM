36ed1f4235cb4a7268549f6e74c211a5acd4c7ab
Qui Hoang <hoangqui11125@gmail.com>
2025-11-21 12:05:49 +0700

    Update calculate fitness
    + Fitness equal to mean of last 0.2 val loss

--- fitness/fitness.py
+++ fitness/fitness.py
@@ -19,17 +19,23 @@ class Fitness():
         model = lstm(input_shape=self.input_shape, params=params)
         optimizer = Adam(learning_rate=self.learning_rate)
         model.compile(optimizer=optimizer, loss='mse')
-        model.fit(
-                x=self.x_train, 
-                y=self.y_train, 
-                validation_data=(self.x_val, self.y_val),
-                epochs=round(params[2]), 
-                batch_size=self.batch_size
-        )
-        y_pred = model.predict(self.x_val)
-        val_loss = mean_squared_error(y_pred, self.y_val)
 
-        # Check if val_loss is NaN or Inf
-        if np.isnan(val_loss).any() or np.isinf(val_loss).any():
-            val_loss = 1
-        return val_loss
+        # Train model và lưu lịch sử loss
+        history = model.fit(
+            x=self.x_train, 
+            y=self.y_train, 
+            validation_data=(self.x_val, self.y_val),
+            epochs=round(params[2]), 
+            batch_size=self.batch_size,
+        )
+
+        n = max(1, round(round(params[2]) * 0.2))
+        val_losses = history.history['val_loss']
+        
+        fitness_val = np.mean(val_losses[-n:])
+
+        # Kiểm tra NaN hoặc Inf
+        if np.isnan(fitness_val) or np.isinf(fitness_val):
+            fitness_val = 1
+
+        return fitness_val
